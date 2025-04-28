775dbff5d157dcc62d9255c4ebd9b56163bfd19e
tyr <tyr0v.k@yandex.ru>
2025-04-28 18:50:20 +0300

    Исправлено задание #2.

--- src/2.kt
+++ src/2.kt
@@ -1,10 +1,10 @@
 fun main() {
-    var nums = arrayListOf<Int>()
-    nums = arrayListOf(8, 21, 5, 90, 11, 0)
-    nums[0] = 90
+    val nums = mutableListOf<Int>()
+    nums.addAll(listOf(8, 21, 5, 90, 11, 0))
+    nums.add(0, 90)
     nums.removeAt(4)
     nums.add(-35)
-    for (i in 0..nums.size-1){
-        print("${nums[i]} ")
+    for (i in nums){
+        print("$i ")
     }
 }
