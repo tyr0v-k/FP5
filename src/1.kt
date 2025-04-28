443f93baf85bbe130be8293aacba6491a8804c8c
tyr <tyr0v.k@yandex.ru>
2025-04-28 18:54:26 +0300

    Исправлено задание #1.

--- src/1.kt
+++ src/1.kt
@@ -1,17 +1,15 @@
-import kotlin.math.abs
-
 fun main() {
     val x = (1..9).random()
     var y = (1..9).random()
     while (x==y){
         y = (1..9).random()
     }
-    val nums = arrayOfNulls<Int>(abs(x-y)+1)
+    val nums = mutableListOf<Int>()
     for (i in (minOf(x, y)..maxOf(x, y))){
-        nums[abs(minOf(x, y) - i)] = i
+        nums.add(i)
     }
     println("${minOf(x,y)}..${maxOf(x,y)}")
-    for (i in 0..nums.lastIndex){
-        print("${nums[i]} ")
+    for (i in nums){
+        print("$i ")
     }
 }
